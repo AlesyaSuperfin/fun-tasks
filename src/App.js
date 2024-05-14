590062c1a75c94eb3d66e648c33b0fcb775a6d03
AlesyaSuperfin <superfin.alesya@gmail.com>
2024-05-14 20:32:15 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import image from './woman-two.jpg';
 
 function App() {
+
+const [task, setTask] = useState(" ");
+
+useEffect ( () => {
+  getTask();
+}, [])
+
+  const getTask = async () => {
+    const response = await fetch (`https://www.boredapi.com/api/activity/`);
+    const data = await response.json();
+    setTask(data.activity)
+  }
+
+  const clicked = () => {
+    getTask();
+    changeCover();
+  }
+
+  const changeCover = () => {
+    const bodyImage = document.querySelector('body');
+    bodyImage.style.backgroundImage = `url('${image}')`;
+
+    const taskField = document.querySelector('.containerTask');
+    taskField.style.display = 'block';
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <h1>Are you bored?
+        <br></br>Don't know what to do?</h1>
+        <button onClick={clicked}>Click here & get a task</button>
+      </div>
+      <div className='containerTask'>
+        <h3>{task}</h3>
+      </div>
     </div>
   );
 }
