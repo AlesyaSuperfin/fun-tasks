f51b040952e2b27aef37eb14f4d08fb956138f66
AlesyaSuperfin <superfin.alesya@gmail.com>
2024-05-17 11:23:37 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import image from './woman-two.jpg';
+import image from './woman-one.jpg';
+import newImage from './woman-two.jpg';
 
 function App() {
 
-const [task, setTask] = useState(" ");
+const [task, setTask] = useState("");
+const [cover, setCover] = useState(image);
 
 useEffect ( () => {
   getTask();
@@ -16,30 +18,36 @@ useEffect ( () => {
     setTask(data.activity)
   }
 
+  const backgroundStyle = {
+    backgroundImage: `url(${cover})`
+  }
+
   const clicked = () => {
     getTask();
-    changeCover();
+    setCover(newImage);
   }
 
-  const changeCover = () => {
-    const bodyImage = document.querySelector('body');
-    bodyImage.style.backgroundImage = `url('${image}')`;
+  // const changeCover = () => {
+  //   const bodyImage = document.querySelector('body');
+  //   bodyImage.style.backgroundImage = `url('${image}')`;
 
-    const taskField = document.querySelector('.containerTask');
-    taskField.style.display = 'block';
-  }
-
+  //   const taskField = document.querySelector('.containerTask');
+  //   taskField.style.display = 'block';
+  // }
 
   return (
-    <div className="App">
+    <div className="App" style={ backgroundStyle }>
+
       <div className='container'>
         <h1>Are you bored?
         <br></br>Don't know what to do?</h1>
         <button onClick={clicked}>Click here & get a task</button>
       </div>
+
       <div className='containerTask'>
         <h3>{task}</h3>
       </div>
+
     </div>
   );
 }
